c218226f84fd95c74bb9f0ecef7c8db64e258380
vovkulakartm <dmyterko.jw@gmail.com>
2022-11-28 04:26:12 +0200

    // ДРУГА ГІЛКА

--- Extension Methods/Program.cs
+++ Extension Methods/Program.cs
@@ -10,7 +10,7 @@
             //ExtantionStaticClass baseClass0 = new(); - не можливо створити екземпляр статичного класу!
 
             resultInnerMethod = baseClass.A_plus_B();
-            resultExtentionMethod = baseClass.ExtentionMethod_A_multiply_AA();   
+            resultExtentionMethod = baseClass.ExtentionMethod_A_multiply_AA();   // ДРУГА ГІЛКА
 
             Console.WriteLine("resultInnerMethod = {0}", resultInnerMethod);
             Console.WriteLine("resultExtentionMethod = {0}", resultExtentionMethod);
